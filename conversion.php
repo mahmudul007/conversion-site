77fb828c22026530e5356d5167542b82efb43d5f
mahmudul007 <62248559+mahmudul007@users.noreply.github.com>
2021-06-23 11:01:57 +0600

    first commit

---
+++ conversion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+
+<body>
+   <p>Page 2 [Conversion]</p> 
+   <p>Conversion site </p>
+   1. <a href="home.php">Home</a> 2. <a href="conversion.php">Conversion Rate</a> 3. <a href="history.php">History</a>
+   </body>
+
+
+      <p>Conversion rate:</p>
+
+<select name="inch_to_feet" id="inch_to_feet">
+  <option value="inch_to_feet">Inch_to_feet</option>
+  
+  <option value="feet_to_inch">feet_to_inch</option>
+  <?php?
+  if ($inch_to_feet) {
+   
+  }
+
+
+
+      </html>
